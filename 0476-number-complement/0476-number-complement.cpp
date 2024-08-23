a8e9f38b74e96378cc14bc99839235294101d012
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-23 18:51:20 +0530

    Time: 0 ms (100%), Space: 7.6 MB (34.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int findComplement(int num) {
+        // int numBits = (int)(log2(num)) + 1;
+        // for(int i=0; i<numBits; i++){
+        //     num = num ^ (1 << i);
+        // }
+        // return num;
+
+        // Approach 2
+
         int numBits = (int)(log2(num)) + 1;
-        for(int i=0; i<numBits; i++){
-            num = num ^ (1 << i);
-        }
-        return num;
+        unsigned int mask = (1U << numBits) - 1;   // gives 2^n-1 as 0111 & then do xor
+
+        return num ^ mask;
     }
 };
