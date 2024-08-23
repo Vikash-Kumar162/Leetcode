bb1b6c4d44694f81a8c35709c5a11aee76782a51
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-23 18:55:01 +0530

    Time: 0 ms (100%), Space: 7.6 MB (34.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -9,9 +9,24 @@ public:
 
         // Approach 2
 
-        int numBits = (int)(log2(num)) + 1;
-        unsigned int mask = (1U << numBits) - 1;   // gives 2^n-1 as 0111 & then do xor
+        // int numBits = (int)(log2(num)) + 1;
+        // unsigned int mask = (1U << numBits) - 1;   // gives 2^n-1 as 0111 & then do xor
 
-        return num ^ mask;
+        // return num ^ mask;
+
+        // Approach 3
+
+        int i = 0;
+        int complement = 0;
+        while(num){
+            if(! (num & 1)){   // if last bit (ith bit) of num is 0, 
+            //then flip ith to 1 , else it's already 0, nothing to do
+                complement |= (1 << i);
+            }
+
+            num >>= 1;
+            i++;
+        }
+        return complement;
     }
 };
