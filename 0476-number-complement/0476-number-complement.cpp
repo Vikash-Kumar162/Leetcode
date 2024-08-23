568b4bc2b13d7e326d48b9f91e6cf98dd8befaf6
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-23 18:46:48 +0530

    Time: 3 ms (41.26%), Space: 7.5 MB (58.43%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     int findComplement(int num) {
-        if(num == 0){
-            return 1;
+        int numBits = (int)(log2(num)) + 1;
+        for(int i=0; i<numBits; i++){
+            num = num ^ (1 << i);
         }
-        if(num == 1){
-            return 0;
-        }
-
-        string s;
-        while(num != 0){
-            if(num & 1) s = '0' + s;
-            else s = '1' + s;
-
-            num = num >> 1;
-        }
-
-        int ans = 0;
-        int p = 0;
-        for(int i=s.size()-1; i>=0; i--){
-            if(s[i] == '1'){
-                ans += pow(2, p);
-            }
-            p ++;
-        }
-        return ans;
+        return num;
     }
 };
