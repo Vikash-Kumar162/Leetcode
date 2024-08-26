fe82b1b70fdc1770ab0c2a65064fb4bfa75079e2
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-26 18:17:34 +0530

    Time: 14 ms (51.7%), Space: 15.2 MB (49.47%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+void traversal(Node* root, vector<int> &postorder){
+    //
+    if(root == NULL){
+        return ;
+    }
+    // traversal(root_children, postorder);
+    // postorder.push_back(root->val);
+
+        for(Node* x : root->children) {
+            traversal(x, postorder);
+        }
+        
+        postorder.push_back(root->val);
+}
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        traversal(root, ans);
+        return ans;
+    }
+};
