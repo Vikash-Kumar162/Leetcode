ac6bad017be8bb6ed80c323f56ddc327084ea0ac
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-27 19:47:30 +0530

    Time: 3 ms (61.73%), Space: 14.9 MB (42.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -27,30 +27,7 @@ vector<int> nextPermutationAns(vector<int>& nums) {
       return nums;
 }
     void nextPermutation(vector<int>& nums) {
-    //   int index = 0;
-    //   int n = nums.size();
-    //   // 1 find dip point 
-    //   for(int i=n-2; i>=0; i--){
-    //     if(nums[i] < nums[i+1]){
-    //         index = i;
-    //         break;
-    //     }
-    //   }  
-    //   // if index = -1 reverse of nums will be ans -> sorted in reverse order
-
-    //   if(index == 0){
-    //     reverse(nums.begin(), nums.end());
-    //   }
-    //   // 2 find someone just greater than dip 
-    //   for(int i=n-1; i>index; i--){
-    //     if(nums[i] > nums[index]){
-    //         swap(nums[i], nums[index]);
-    //         break;
-    //     }
-    //   }
-    //   reverse(nums.begin() + index + 1, nums.end());
-
+        
     nums = nextPermutationAns(nums);
-
     }
 };
