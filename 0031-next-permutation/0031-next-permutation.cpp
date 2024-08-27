3d8e071240cd9c272d14c063f2794a11ad4b4a0c
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-08-27 19:46:55 +0530

    Time: 6 ms (17.88%), Space: 15 MB (6.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,56 @@
 class Solution {
 public:
+vector<int> nextPermutationAns(vector<int>& nums) {
+       int index = -1;
+      int n = nums.size();
+      // 1 find dip point 
+      for(int i=n-2; i>=0; i--){
+        if(nums[i] < nums[i+1]){
+            index = i;
+            break;
+        }
+      }  
+      // if index = -1 reverse of nums will be ans -> sorted in reverse order
+
+      if(index == -1){
+        reverse(nums.begin(), nums.end());
+        return nums;
+      }
+      // 2 find someone just greater than dip 
+      for(int i=n-1; i>index; i--){
+        if(nums[i] > nums[index]){
+            swap(nums[i], nums[index]);
+            break;
+        }
+      }
+      reverse(nums.begin() + index + 1, nums.end());
+      return nums;
+}
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+    //   int index = 0;
+    //   int n = nums.size();
+    //   // 1 find dip point 
+    //   for(int i=n-2; i>=0; i--){
+    //     if(nums[i] < nums[i+1]){
+    //         index = i;
+    //         break;
+    //     }
+    //   }  
+    //   // if index = -1 reverse of nums will be ans -> sorted in reverse order
+
+    //   if(index == 0){
+    //     reverse(nums.begin(), nums.end());
+    //   }
+    //   // 2 find someone just greater than dip 
+    //   for(int i=n-1; i>index; i--){
+    //     if(nums[i] > nums[index]){
+    //         swap(nums[i], nums[index]);
+    //         break;
+    //     }
+    //   }
+    //   reverse(nums.begin() + index + 1, nums.end());
+
+    nums = nextPermutationAns(nums);
+
     }
 };
