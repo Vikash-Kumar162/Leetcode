b0b3212514f1c2b5bcb85d7676bd3ab29f7e1031
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2024-12-26 12:46:47 +0530

    Time: 7 ms (51.66%), Space: 15.8 MB (9.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-static bool comp(pair<int,int>& a, pair<int,int>& b){
-    return a.first < b.first;
-}
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> data;
-        // vector<int> ans;
+        unordered_map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
-            int val = nums[i];
-            int index = i;
-            data.push_back({val, index});
+            mp[nums[i]] = i;
         }
-        // custom sort
-        sort(data.begin(), data.end(), comp);
-
-        int s = 0, e = data.size()-1;
-        while(s < e){
-            int sum = data[s].first + data[e].first;
-            if(sum == target){
-                // ans.push_back(data[s].second);
-                // ans.push_back(data[e].second);
-                // break;
-                return {data[s].second, data[e].second};
+        for(int i=0; i<nums.size(); i++){
+            int newTarget = target - nums[i];
+            if(mp.find(newTarget) != mp.end() && mp[newTarget] != i){
+                return {i, mp[newTarget]};
             }
-            else if(sum < target)
-            s ++;
-            else
-            e--;
         }
         return {};
     }
