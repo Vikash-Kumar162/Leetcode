3d424ffed68723d46a316c4c2ff234d7ce54966f
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2025-12-06 10:01:32 +0530

    Time: 103 ms (21.54%), Space: 138.7 MB (6.62%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,29 +1,5 @@
 class Solution {
 public:
-int solve(vector<int>& nums1, vector<int>& nums2, int index, bool swapped){
-    // Base Case
-    if(index == nums1.size()){
-        return 0; //no swap need at last index as last elem already sorted
-    }
-
-    int prev1 = nums1[index-1];
-    int prev2 = nums2[index-1];
-    int ans = INT_MAX;
-    if(swapped){
-        swap(prev1, prev2);
-    }
-
-    // No swap
-    if(nums1[index] > prev1 && nums2[index] > prev2){
-        ans = solve(nums1, nums2, index+1, 0);
-    }
-    // Swap
-    if(nums1[index] > prev2 && nums2[index] > prev1){
-        ans = min(ans, 1+solve(nums1, nums2, index+1, 1));
-    }
-    return ans;
-}
-
 int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swapped, vector<vector<int>>& dp){
     // Base Case
     if(index == nums1.size()){
