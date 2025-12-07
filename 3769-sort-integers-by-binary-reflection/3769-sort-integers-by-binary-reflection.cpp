2d4ded0f408b8c266281f8c81d185402dd6ea5e1
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2025-12-07 12:44:11 +0530

    Time: 15 ms (41.07%), Space: 38.4 MB (27.67%) - LeetHub

--- 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     vector<int> sortByReflection(vector<int>& nums) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, 
-                   greater<pair<int,int>>> pq;
 
-    for(int i = 0; i < nums.size(); i++){
-        
-        bitset<32> binary_representation(nums[i]); 
-        string s = binary_representation.to_string();
-
-        // Remove leading zeros
-        s = s.substr(s.find('1'));
-
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-
-        // Convert reversed binary to int safely
-        long long dec = 0;
-        for (char c : rev) {
-            dec = dec * 2 + (c - '1' + 1);   // dec = dec * 2 + (c == '1');
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for(int i=0; i<nums.size(); i++){
+            bitset<32> binary_representation(nums[i]); 
+            string s = binary_representation.to_string();
+            s = s.substr(s.find('1'));
+            string rev = s;
+            reverse(rev.begin(), rev.end());
+            int dec = stoi(rev, nullptr, 2);
+            pq.push({dec, nums[i]});
         }
-
-        // Store reflection + original number for tie-breaking
-        pq.push({(int)dec, nums[i]});
-    }
-
-    vector<int> ans;
-    while(!pq.empty()){
-        ans.push_back(pq.top().second);  // return original number
-        pq.pop();
-    }
-
-    return ans;
+        vector<int> ans;
+        while(!pq.empty()){
+            auto el = pq.top();
+            ans.push_back(el.second);
+            pq.pop();
+        }
+       return ans;
     }
 };
