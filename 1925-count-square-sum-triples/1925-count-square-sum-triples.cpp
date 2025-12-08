5610e055d0ac824c444e39cb51d14f65f0031d91
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2025-12-08 09:35:37 +0530

    Time: 230 ms (16.15%), Space: 7.9 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -6,7 +6,7 @@ public:
         for(int a=1; a<=n; a++){
             for(int b=1; b<=n; b++){
                 for(int c=1; c<=n; c++){
-                    if(a!=b && b!=c && a!=c && (a*a + b*b == c*c))
+                    if(a*a + b*b == c*c)
                     count ++;
                 }
             }
