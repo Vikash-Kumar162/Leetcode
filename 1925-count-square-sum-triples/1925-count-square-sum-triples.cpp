f5c938b087b64fc63cc907865c2d23ac4e035665
Vikash Kumar Yadav <158133611+Vikash-Kumar162@users.noreply.github.com>
2025-12-08 09:42:05 +0530

    Time: 35 ms (32.96%), Space: 8.8 MB (10.18%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -2,13 +2,29 @@ class Solution {
 public:
     int countTriples(int n) {
         // Brute Force
+        // int count = 0;
+        // for(int a=1; a<=n; a++){
+        //     for(int b=1; b<=n; b++){
+        //         for(int c=1; c<=n; c++){
+        //             if(a*a + b*b == c*c)
+        //             count ++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        // BETTER APPROACH
+        set<int> st;
+        for(int i=1; i<=n; i++){
+            st.insert(i*i);
+        }
+
         int count = 0;
         for(int a=1; a<=n; a++){
             for(int b=1; b<=n; b++){
-                for(int c=1; c<=n; c++){
-                    if(a*a + b*b == c*c)
-                    count ++;
-                }
+                int square = a*a + b*b;
+                if(st.find(square) != st.end())
+                count ++;
             }
         }
         return count;
